d0a69f13aa3ffc3bff7115f527366b30a96c023a
Alberto Grespan <alberto@albertogrespan.com>
2017-11-11 16:23:42 -0400

    Use Go Kit's logger

--- cmd/refresh/main.go
+++ cmd/refresh/main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"flag"
-	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/go-kit/kit/log"
 	"gitlab.full360.com/full360/refresh"
 )
 
 func main() {
 	s3Bucket := flag.String("s3-bucket", "dev.playground.prometheus", "Name of the AWS S3 Bucket")
+	s3BucketPrefix := flag.String("s3-bucket-prefix", "", "Name of the AWS S3 Bucket Prefix")
 	awsRegion := flag.String("aws-region", "us-east-1", "AWS Region")
 	downloadDir := flag.String("download-dir", "something", "Download directory")
 
 	// log setup
-	logger := log.New(os.Stderr, "", log.Ldate|log.Ltime)
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(os.Stderr)
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
 
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String(*awsRegion),
@@ -27,15 +30,18 @@ func main() {
 	downloader := &refresh.Downloader{
 		Svc:     s3.New(sess),
 		Manager: s3manager.NewDownloader(sess),
-		Logger:  logger,
+		Logger:  log.With(logger, "service", "s3Service"),
 		Config: struct {
 			Bucket string
 			Prefix string
 		}{
 			Bucket: *s3Bucket,
-			Prefix: "",
+			Prefix: *s3BucketPrefix,
 		},
 	}
 
-	downloader.Download(*downloadDir)
+	if err := downloader.Download(*downloadDir); err != nil {
+		logger.Log("msg", err.Error())
+	}
+
 }
--- s3_dispatcher.go
+++ s3_dispatcher.go
@@ -2,19 +2,19 @@ package refresh
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/go-kit/kit/log"
 )
 
 type Downloader struct {
 	Svc     *s3.S3
 	Manager *s3manager.Downloader
-	Logger  *log.Logger
+	Logger  log.Logger
 	Config  struct {
 		Bucket string
 		Prefix string
@@ -33,18 +33,18 @@ func (s *Downloader) Download(downloadDir string) error {
 			// Create the directories in the path
 			file := filepath.Join(downloadDir, *obj.Key)
 			if err := os.MkdirAll(filepath.Dir(file), os.ModePerm); err != nil {
-				fmt.Println("WHAT")
+				s.Logger.Log("msg", err.Error())
 			}
 
 			// Setup the local file
 			fd, err := os.Create(file)
 			if err != nil {
-				fmt.Println("CREATE")
+				s.Logger.Log("msg", err.Error())
 			}
 
 			defer fd.Close()
 
-			fmt.Printf("Downloading s3://%s/%s to %s...\n", s.Config.Bucket, *obj.Key, file)
+			s.Logger.Log("msg", fmt.Sprintf("downloading s3://%s/%s to %s", s.Config.Bucket, *obj.Key, file))
 			s.Manager.Download(fd, &s3.GetObjectInput{Bucket: &s.Config.Bucket, Key: obj.Key})
 		}
 		return true
